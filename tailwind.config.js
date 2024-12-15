000949a69f2a6ad12781b02c2919a0e5ef5e12c0
DikshantJatrana <dikshantjatrana02@gmail.com>
2024-12-15 18:36:03 +0530

    Adding neccessary Fonts/Images and defining colors and File Structure

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,24 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+module.exports = {
+  content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primaryBlue: "#00E0FF",
+        primaryPink: "#FF48B0",
+        primaryYellow: "#FFD500",
+        primaryGrayDark: "#3A3A3A",
+        primaryGrayLight: "#A5A5A5",
+        primaryWhite: "#F5F5F5",
+      },
+      backgroundImage: {
+        "custom-gradient":
+          "linear-gradient(90deg, rgba(255,213,0,1) 5%, rgba(255,72,176,1) 50%, rgba(0,224,255,1) 95%)",
+      },
+      fontFamily: {
+        tanker: ["Tanker", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
